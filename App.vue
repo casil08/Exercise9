23942944b793e4c2af1522d951dda6efd9aee536
casil08 <156186206+casil08@users.noreply.github.com>
2024-08-19 17:34:15 +0800

    Fetch Data from an API in Vue.js

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Hello, Vue!</h1>
+    <DataComponent />
+  </div>
+</template>
+
+<script>
+import DataComponent from './DataComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    DataComponent
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ DataComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <h2>Posts</h2>
+      <ul>
+    </div>
+        <li v-for="post in posts" "key="post.id">{{ post.title}}</li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            posts: []
+        };
+    },
+    created () {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => {
+            this.posts = data;
+        });
+    }
+};
+</script>
